0fadc4cbba5efdf93befd25ca5795330b7fa819f
Pooh <tatakornaow@gmail.com>
2022-12-27 15:56:51 +0700

    lab 10_2 done

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 //Write the function printO() here
 
+int printO(int a,int b)
+{
+    if (a<=0 || b<=0) cout << "Invalid input";
+    else
+    {
+        for(int i=0; i<a; i++)
+        {
+            for(int i=0; i<b; i++)
+            {
+                cout << "O";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
 int main(){
 	
 	printO(2,2);
